38f8bf94365a65a5e7aa17b0314b571cca9870ba
Kostiantyn Butenko <costya.butenko@gmail.com>
2023-09-05 14:45:10 +0300

    firs commit

---
+++ app/page.tsx
@@ -0,0 +1,59 @@
+import { CarCard, CustomFilter, Hero, SearchBar } from '@/components'
+import { fuels, yearsOfProduction } from '@/constants';
+import { fetchCars } from '@/utils'
+import Image from 'next/image'
+
+export default async function Home({ searchParams }) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2023,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || '',
+    model: searchParams.model || '',
+  });
+
+  const isDataEmpty = !Array.isArray(allCars) || 
+  allCars.length <1 || !allCars;
+
+  return (
+    <main className="overflow-hidden">
+       <Hero />
+
+       <div className='mt-12 padding-x padding-y
+       max-width' id='discover'>
+         <div className='home__text-container'>
+           <h1 className='text-4xl font-extrabold'>
+             Car Catalogue
+           </h1>
+           <p>Explore the car you might like</p>
+         </div>
+
+         <div className='home__filters'>
+           <SearchBar />
+
+           <div className='home__filter-container'>
+              <CustomFilter  title='fuel' options={fuels}/>
+              <CustomFilter  title='year' options={yearsOfProduction}/>
+           </div>
+         </div>
+         
+         {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => ( 
+              <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+         ): (
+          <div className='home__error-container'>
+            <h2 className='text-balck text-xl
+            font-bold'>Oooops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+         )}
+
+       </div>
+    </main>
+  )
+}
